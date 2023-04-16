d74211f69fe307589a23ca45b2c8422aaff91b7e
canning1295 <129696332+canning1295@users.noreply.github.com>
2023-04-16 16:15:46 -0500

    Setup Project

---
+++ index.js
@@ -0,0 +1,16 @@
+export const options = {
+    url: 'https://canning.herokuapp.com/',
+    dateStart: new Date('2022-12-08T00:00'),
+    dateEnd: new Date('2022-12-15T00:00'),
+    showBasalChart: false,
+    showBGChart: true,
+    showCOBChart: false,
+    COBRate: 30,
+    adjustBasalRates: true,
+    ISF: 27,
+    weight: 80,
+    minBG: NaN,
+    targetBG: 110,
+    poolingTime: 120,
+    period: 30
+  }
