b757d7e95c2bf06549339ee7b2ca551f8944f54b
Canning <canning1295@gmail.com>
2023-05-03 14:27:01 -0500

    Adjusted GIR Curves.

--- calculations/DIA.js
+++ calculations/DIA.js
@@ -1,4 +1,4 @@
- import { options } from '../index.js';
+import { options } from '../index.js';
 import * as GIR from './GIR.js';
 
 //This getDIA function returns DIACurves where each position contains an array that can vary in length based on the amount of insulin delivered. Each item in the array is the percentage of insulin used/infused over a 5 minute period.
@@ -9,7 +9,7 @@ export function getDIA(netBasals, insulinAdjustment) {
     for (let i = 0; i < 288; i++) {
         insulinDeliveredArr[i] = netBasals[i];
     }
-
+    let maxIndexArray = new Array(288).fill([]);
     let DIACurves = new Array(288).fill([]);
     for (let i = 0; i < 288; i++) {
         let insulin = 0;
@@ -21,6 +21,8 @@ export function getDIA(netBasals, insulinAdjustment) {
             insulin += insulinDeliveredArr[index];
         }
         let GIRCurve = GIR.GIRCurve((insulin + insulinAdjustment) / options.weight);
+        // This calculates where the peak of the curve, but / by 20 for the 5 minute intervals
+        maxIndexArray[i] = (GIRCurve.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0)) / 20;
         let GIRCurvePercents = [];
         let GIRCurveTotal = 0;
         for (let i = 0; i < GIRCurve.length; i++) {
@@ -42,62 +44,20 @@ export function getDIA(netBasals, insulinAdjustment) {
     for (let i = 0; i < 288; i++) {
         shortenedDIACurves[i] = shortenCurve(i, DIACurves)
     }
-      
+
+    // When adding insulin, the insulin doesn't actually affect the whole day, but rather only impacts BG for the duration of insulin action. Accordingly, at the end of the DIA period, the calculations indicate the 100% of the insulin has been used, so the last BG of that period ends up being much lower (typically) than the next BG. To make it easier to fine tunue insulin adjustments. We add a short period onto the end of the DIACurves. Although this lengthens the DIACurves, thereby making the predictions less aggressive, it still allows for more accurate insulin adjustments.
+    for (let i =0; i < 288; i++) {
+        for(let j = 0; j < maxIndexArray[i].length; j++) {
+            100 - shortenedDIACurves[i].push(shortenedDIACurves[i][j])
+            j += 100 / maxIndexArray[i]
+        }
+    }
     return shortenedDIACurves;
 }
 function shortenCurve(i, DIACurves) {
     let shortenedCurve = [];
-    for (let j = 0; j < DIACurves[i].length; j += 20) {
-        shortenedCurve.push(DIACurves[i][j]);
+    for (let k = 0; k < DIACurves[i].length; k += 20) {
+        shortenedCurve.push(DIACurves[i][k]);
     }
     return shortenedCurve;          
 }
-
-
-//This getDIA function returns DIACurves where each position contains an array that can vary in length based on the amount of insulin delivered. Each item in the array is the percentage of insulin used/infused over a 15 second period.
-// 
-
-//This is getDIA, but using 576 positions to assist with some calculations. It is not currently used as I wrote a new getDIA function that uses 288 positions and loops back on itself.
-// export function getDIA(averageCombinedData, insulinAdjustment) {
-//     console.log('averageCombinedData', averageCombinedData)
-//     let netBasals = averageCombinedData.map((data) => data.actualBasal);
-
-//     let insulinDeliveredArr = new Array(576) // Although there are only 288 5 minute periods in a day, we add 144 positions on the front and back to assist with some caluculations.
-//     for (let i = 0; i < 144; i++) {
-//             insulinDeliveredArr[i] = netBasals[i + 144];
-//             insulinDeliveredArr[i + 144] = netBasals[i];
-//             insulinDeliveredArr[i + 288] = netBasals[i + 144];
-//             insulinDeliveredArr[i + 432] = netBasals[i];
-//     }
-    
-//     // console.log('insulinDeliveredArr', insulinDeliveredArr)
-//     let DIACurves = new Array(576).fill([])
-//     // let GIRCurve = []
-//         for (let i = 144; i < 432; i++) {
-//                 let insulin = 0;
-//                 for (let j = i - options.poolingTime / 5; j < i; j++) {
-//                 insulin += insulinDeliveredArr[j];
-//                 }
-//                 let GIRCurve = GIR.GIRCurve((insulin + insulinAdjustment) / options.weight) // This is the curve for how insulin delivered during this 5 minute period will affect BG over time.
-//                 let GIRCurvePercents = []
-//                 let GIRCurveTotal = 0
-//                 for(let i = 0; i < GIRCurve.length; i++) {
-//                 GIRCurveTotal += GIRCurve[i]
-//                 }
-//                 let currentTotal = 0
-//                 for(let i = 0; i < GIRCurve.length; i++) {
-//                 currentTotal += GIRCurve[i]
-//                 if(i == 0) {GIRCurvePercents.push(0)} // Make first value 0
-//                 else {GIRCurvePercents.push(currentTotal / GIRCurveTotal)}
-
-//                 }
-//                 DIACurves[i] = GIRCurvePercents // This turns the curve into the percent of insulin used at each 5 minute period.
-//         }
-//         for (let i = 0; i < 144; i++) {
-//             DIACurves[i] = DIACurves[i + 288];
-//             DIACurves[i + 432] = DIACurves[i + 144];
-//         }
-//     return DIACurves
-// }
-
-
